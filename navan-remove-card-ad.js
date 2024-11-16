07711c7101bd8cb0d60b46657fe019cd321ac9a5
André Srinivasan <andre.srinivasan@gmail.com>
2024-11-16 14:49:58 -0800

    Improve error handling
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- navan-remove-card-ad.js
+++ navan-remove-card-ad.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Remove Navan Card Ad
 // @description Don't show Navan ads for their card
-// @version     0.0.5
+// @version     0.0.6
 // @license     The Unlicense
 // @author      André Srinivasan
 // @website     https://github.com/andresrinivasan/userscripts
@@ -18,13 +18,13 @@ VM.observe(document.body, () => {
     }
   
     const n2 = document.querySelector(".order-card-cta-container[_ngcontent-ng-c3120622055]");
-    if (n2.style.display != "none") {
+    if (n2 != null && n2.style.display != "none") {
       n2.style.setProperty("display", "none", "important")
     }
 
     // Added when Order Card CTA came back. Not sure if the one above is still needed
     const n3 = document.querySelector("user-order-new-card-tile.ng-star-inserted");
-    if (n3.style.display != "none") {
+    if (n3 != null && n3.style.display != "none") {
       n3.style.setProperty("display", "none", "important")
     }
 });
