45c5f5d94344a72c99409c2103b3e722c83a5f43
André Srinivasan <andre.srinivasan@gmail.com>
2024-12-28 11:17:20 -0800

    Unsubscribe from observable
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- navan-remove-card-ad.js
+++ navan-remove-card-ad.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Remove Navan Card Ads
 // @description Don't show Navan ads for their card
-// @version     0.0.8
+// @version     0.0.9
 // @license     The Unlicense
 // @author      André Srinivasan
 // @website     https://github.com/andresrinivasan/userscripts
@@ -11,22 +11,29 @@
 // @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // ==/UserScript==
 
+// Need to use VM.observe to wait for the CTA to show up
+
 VM.observe(document.body, () => {
-    // Remove Navan Card CTA across the top of the page
-    const n1 = document.querySelector(".order-card-cta-left-container");
-    if (n1) {
-      document.querySelector(".secondary").click()
+  let done = false;
+
+  // Remove Navan Card CTA across the top of the page
+  const n1 = document.querySelector(".order-card-cta-left-container");
+  if (n1) {
+    document.querySelector(".secondary").click();
+    done = true;
+  }
+
+  // Remove Navan Card CTA on the right nav
+  const containerCSSPath = [
+    ".order-card-cta-container[_ngcontent-ng-c3120622055]", 
+    "user-order-new-card-tile.ng-star-inserted",
+  ];
+  for (const path of containerCSSPath) {
+    const n2 = document.querySelector(path);
+    if (n2 != null && n2.style.display != "none") {
+      n2.style.setProperty("display", "none", "important");
     }
-  
-    // Remove Navan Card CTA on the right nav
-    const containerCSSPath = [
-      ".order-card-cta-container[_ngcontent-ng-c3120622055]", 
-      "user-order-new-card-tile.ng-star-inserted",
-    ];
-    for (const path of containerCSSPath) {
-      const n2 = document.querySelector(path);
-      if (n2 != null && n2.style.display != "none") {
-        n2.style.setProperty("display", "none", "important")
-      }
-    }
+  }
+
+  return done;
 });
