a3ba4d3b5d2e0d385ee4bc32fd9089cc25a92b9e
André Srinivasan <andre.srinivasan@gmail.com>
2025-09-12 18:18:43 -0700

    First version
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

---
+++ clickup-nav-cleanup.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Clickup Nav Cleanup
+// @version      1.0.1
+// @description  Remove parts of the nav bar that I don't care about
+// @author       andresrinivasan
+// @license      Unlicense
+// @match        https://app.clickup.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=app.clickup.com
+// @require      https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
+// @downloadURL  https://github.com/andresrinivasan/userscripts/raw/main/clickup-nav-cleanup.js
+// @supportURL   https://github.com/andresrinivasan/userscripts/issues
+// ==/UserScript==
+
+VM.observe(document.body, () => {
+    const selectors = [
+        "#app-root > cu-app-view-deferred > cu-app-view > cu-app-shell > cu-manager > div.cu-manager2__container.ng-tns-c2866496296-4.ng-star-inserted.cu-manager2__container_v3.layout-size_compact > div > div > cu-simple-bar > div > div > div.cdk-virtual-scrollable.ng-tns-c2032951352-6.cu-simple-bar__body > div:nth-child(3)",
+        "#app-root > cu-app-view-deferred > cu-app-view > cu-app-shell > cu-manager > div.cu-manager2__container.ng-tns-c2866496296-4.ng-star-inserted.cu-manager2__container_v3.layout-size_compact > div > div > cu-simple-bar > div > div > div.cdk-virtual-scrollable.ng-tns-c2032951352-6.cu-simple-bar__body > div.ng-tns-c2032951352-6.cu-simple-bar__item-container.cu-simple-bar__item-container_home.ng-star-inserted",
+        "#app-root > cu-app-view-deferred > cu-app-view > cu-app-shell > cu-manager > div.cu-manager2__container.ng-tns-c2866496296-4.ng-star-inserted.cu-manager2__container_v3.layout-size_compact > div > div > cu-simple-bar > div > div > div.cdk-virtual-scrollable.ng-tns-c2032951352-6.cu-simple-bar__body > div.ng-tns-c2032951352-6.cu-simple-bar__item-container.ng-star-inserted",
+        "#app-root > cu-app-view-deferred > cu-app-view > cu-app-shell > cu-manager > div.cu-manager2__container.ng-tns-c2866496296-4.ng-star-inserted.cu-manager2__container_v3.layout-size_compact > div > div > cu-simple-bar > div > div > div.cdk-virtual-scrollable.ng-tns-c2032951352-6.cu-simple-bar__body > cu-sidebar-favorites"
+    ];
+
+    for (const s of selectors) {
+        try {
+            document.querySelector(s).remove();
+        } catch (e) {
+            return false;
+        }
+    }
+
+    return true;
+});
