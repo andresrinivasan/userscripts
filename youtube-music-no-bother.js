1f8d0c7baa26427c5db6e95ef1eafd0f144eef3c
André Srinivasan <andre.srinivasan@gmail.com>
2026-02-17 12:20:53 -0800

    Refactor popup button observation logic to wait for document to load.
    Signed-off-by: André Srinivasan <andre.srinivasan@gmail.com>

--- youtube-music-no-bother.js
+++ youtube-music-no-bother.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Music I'm still here listening
-// @version      2.0.0
+// @version      2.1.0
 // @description  Forked from https://greasyfork.org/en/scripts/535841-youtube-music-i-m-still-here-listening. Clicks on the annoying button for you and trying to fake page focus so this message won't appear at all
 // @author       andresrinivasan
 // @author       kkrow
@@ -45,21 +45,24 @@
         return origWindowAddEventListener.apply(this, arguments);
     };
 
-    // Click popup buttons as soon as they appear.
-    VM.observe(document.body, () => {
-        // "Are you still listening?" - Yes button
-        const stillHereBtn = document.querySelector('ytmusic-you-there-renderer .yt-spec-button-shape-next--call-to-action, ytmusic-you-there-renderer yt-button-renderer button');
-        if (stillHereBtn) {
-            stillHereBtn.click();
-        }
+    // Wait for the body to exist before observing it.
+    window.addEventListener('DOMContentLoaded', (event) => {
+        // Click popup buttons as soon as they appear.
+        VM.observe(document.body, () => {
+            // "Are you still listening?" - Yes button
+            const stillHereBtn = document.querySelector('ytmusic-you-there-renderer .yt-spec-button-shape-next--call-to-action, ytmusic-you-there-renderer yt-button-renderer button');
+            if (stillHereBtn) {
+                stillHereBtn.click();
+            }
 
-        // "Upgrade" or other promos - Dismiss button
-        const promoDismissBtn = document.querySelector('ytmusic-mealbar-promo-renderer .dismiss-button, .yt-spec-button-shape-next--call-to-action-inverse');
-        if (promoDismissBtn) {
-            promoDismissBtn.click();
-        }
+            // "Upgrade" or other promos - Dismiss button
+            const promoDismissBtn = document.querySelector('ytmusic-mealbar-promo-renderer .dismiss-button, .yt-spec-button-shape-next--call-to-action-inverse');
+            if (promoDismissBtn) {
+                promoDismissBtn.click();
+            }
 
-        // Popups can come back, so we keep observing.
-        return false;
+            // Popups can come back, so we keep observing.
+            return false;
+        });
     });
 })();
